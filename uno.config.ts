d6febfeb2aed9f1402771f6166a05e6b9bb03e1a
Lance Guyatt <lanceguyatt@gmail.com>
2022-12-14 20:37:49 +0000

    typo

--- uno.config.ts
+++ uno.config.ts
@@ -9,17 +9,19 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    { 'foo': 'w-full mx-auto px-4 lg:px-10 max-w-[80rem] border' },
-    { 'c-h0': 'text-[5rem] leading-none tracking-[-0.01em] md:text-[6.25rem] lg:text-[7.5rem]' },
-    { 'c-h1': 'text-[3.75rem] leading-none' },
-    { 'c-h2': 'text-[5rem] leading-none' },
-    { 'c-h3': 'text-[3rem] leading-none' },
-    { 'c-label': 'text-base leading-body' },
-    { 'c-link': 'text-[1.625rem] leading-none' },
-    { 'c-link-large': 'text-[3.75rem] leading-none' },
+    { 'foo': 'w-full mx-auto px-4 lg:px-10 max-w-[80rem]' },
+    { 'h0': 'text-[5rem] font-normal leading-none tracking-[-0.01em] md:text-[6.25rem] lg:text-[7.5rem]' },
+    { 'h1': 'text-[3.75rem] font-normal leading-none lg:text-[5rem]' },
+    { 'h2': 'text-[2.5rem] font-normal leading-none lg:text-[3.75rem]' },
+    { 'h3': 'text-[3rem] font-normal leading-none' },
+    { 'label': 'text-base leading-body' },
+    { 'link': 'text-[1.625rem] leading-none' },
+    { 'link-large': 'text-[3.75rem] leading-none' },
     { 'button': 'text-[1.125rem] leading-none inline-block p-3 border-2' },
     { 'button--primary': 'bg-yellow' },
-    { 'lead': 'text-[3rem]' }
+    { 'lead': 'text-[3rem]' },
+    { 'body': 'text-24px leading-[calc(32/24)] lg:text-26px lg:leading-[calc(34/26)]' },
+    { 'badge': '' }
   ],
   transformers: [
     transformerDirectives(),
@@ -30,8 +32,20 @@ export default defineConfig({
     presetTypography({
       cssExtend: {
         h1: {
-          'font-size': '12px',
+          'font-size': '',
+          'font-weight': 'normal',
+          'line-height': 1,
+        },
+        h2: {
+          'font-weight': 'normal',
           'line-height': 1
+        },
+        h3: {
+          'font-weight': 'normal',
+          'line-height': 1
+        },
+        p: {
+          'line-height': '1.33333333'
         },
         'hr': {
           color: 'red'
@@ -52,13 +66,14 @@ export default defineConfig({
       lg: '1280px'
     },
     fontFamily: {
-      sans: "'neue-haas-grotesk-display', sans-serif",
+      sans: ['neue-haas-grotesk-display', 'sans-serif'],
     },
     spacing: {
       foo: '19.375rem',
     },
     colors: {
-      yellow: 'rgba(245, 255, 81, 1)'
+      yellow: 'rgba(245, 255, 81, 1)',
+      red: '#FF5C00',
     }
   }
 })

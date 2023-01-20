ff778e51668fec423328303f3cfa7c86bf8ab1af
Lance Guyatt <lanceguyatt@gmail.com>
2023-01-20 09:56:47 +0000

    manifesto

---
+++ src/components/Foo.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    console.log('Mounted')
+  })
+
+  function click() {
+    alert('Clicked!')
+  }
+</script>
+
+<div class="p-5 bg-yellow text-black">
+  <button type="button" on:click={click}>Click me</button>
+</div>

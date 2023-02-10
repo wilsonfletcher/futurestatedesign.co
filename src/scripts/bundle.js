09070f5590fdefb87363affb075063d978ede4a9
Lance Guyatt <lanceguyatt@gmail.com>
2023-02-10 20:43:55 +0000

    alpinejs

---
+++ src/scripts/bundle.js
@@ -0,0 +1,12 @@
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+
+// import modal from './modal'
+
+// Alpine.data('modal', modal)
+
+Alpine.plugin(collapse)
+
+window.Alpine = Alpine
+
+Alpine.start()
